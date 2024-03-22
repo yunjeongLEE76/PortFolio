cf0a413f604f7b89d76f3839fe7e92747442b29e
yunjeongLEE76 <98930874+yunjeongLEE76@users.noreply.github.com>
2024-03-23 02:45:19 +0900

    typing

--- js/script.js
+++ js/script.js
@@ -18,6 +18,29 @@ window.addEventListener("mousemove", () => {
   // y = event.pageY - window.innerHeight / 2;
 });
 
+let scrollNum = 0;
+let documentHeight = 0;
+const contWrap = document.querySelector(".contWrap");
+const coverHeight = window.innerHeight;
+const container = document.querySelector(".container");
+
+// coverWrap.style.height = `${coverHeight}px`;
+// contWrap.style.marginTop = `${coverHeight}px`;
+
+window.addEventListener("scroll", () => {
+  scrollNum = window.scrollY;
+  documentHeight =
+    document.body.scrollHeight + coverHeight - window.innerHeight;
+
+  if (scrollNum >= documentHeight) {
+    container.style.backgroundColor = `rgba(0, 0, 0, 0)`;
+  } else {
+    container.style.backgroundColor = `rgba(0, 0, 0, ${
+      0.2 + scrollNum / 1000
+    })`;
+  }
+});
+
 const loop = () => {
   targetX += (x - targetX) * speed;
 
@@ -29,3 +52,40 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 };
 loop();
+
+// const progressBar = document.querySelector(".bar");
+// const header = document.querySelector("header");
+// const coverTitle = document.querySelector(".coverTitle");
+// const coverWrap = document.querySelector(".coverWrap");
+// const dimd = document.querySelector(".dimd");
+// const contWrap = document.querySelector(".contWrap");
+
+// let scrollNum = 0;
+// let documentHeight = 0;
+// let per = 0;
+// const coverHeight = window.innerHeight;
+
+// coverWrap.style.height = `${coverHeight}px`;
+// contWrap.style.marginTop = `${coverHeight}px`;
+
+// window.addEventListener("scroll", () => {
+//   scrollNum = window.scrollY;
+//   documentHeight =
+//     document.body.scrollHeight + coverHeight - window.innerHeight;
+
+//   per = percent(scrollNum, documentHeight);
+//   progressBar.style.width = `${per}%`;
+
+//   if (scrollNum >= coverHeight) {
+//     header.classList.add("fixed");
+//   } else {
+//     header.classList.remove("fixed");
+//     coverTitle.style.top = `${-scrollNum / 10}px`;
+//     coverWrap.style.backgroundPosition = `center ${-scrollNum / 4}px`;
+//     dimd.style.backgroundColor = `rgba(0, 0, 0, ${0.4 + scrollNum / 1000})`;
+//   }
+// });
+
+// const percent = (num, totalNum) => {
+//   return ((num / totalNum) * 100).toFixed(0);
+// };
